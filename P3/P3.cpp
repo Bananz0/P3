6b3a990abd12fb26d7a9fab3309cfd5fc2394e9b
glenm <glenm@XPS>
2024-02-14 17:49:37 +0000

    Add project files.

---
+++ P3/P3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+	class complexNum {
+	public:
+		
+		complexNum();
+		complexNum(double real, double imaginary);
+
+		double getReal() {
+			return imaginary;
+		}
+		double getImaginary(){
+			return imaginary;
+		}
+			;
+
+	private: 
+		double real = 0, imaginary = 0, absoluteVal = 0, congugate= 0 ;
+		
+		
+		
+		
+	};
+}
