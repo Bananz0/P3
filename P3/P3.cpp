982840feb159f3b64e71df2c8e7e77254d890fe4
glenm@XPS <glenmuthokaq@gmail.com>
2024-02-14 19:36:08 +0000

    Headerized it

--- P3/P3.cpp
+++ P3/P3.cpp
@@ -1,70 +1,6 @@
 #include <iostream>
 
-class complexNum {
-public:
-	complexNum() : real(0), imaginary(0) {}
-	complexNum(double realIn, double imaginaryIn) : real(realIn), imaginary(imaginaryIn) {}
-
-	double getReal() {
-		return real;
-	}
-	double getImaginary() {
-		return imaginary;
-	}
-	double getModulus() {
-		return sqrt(pow(imaginary, 2) + pow(real, 2));
-	}
-	double getPhase() {
-		return atan2(imaginary, real);
-	}
-
-private:
-	double real = 0, imaginary = 0, absoluteVal = 0, congugate = 0;
-};
-const complexNum operator +(complexNum& uno, complexNum& dos) {
-	double realSum = uno.getReal() + dos.getReal();
-	double imagSum = uno.getImaginary() + dos.getImaginary();
-	return complexNum(realSum, imagSum);
-}
-const complexNum operator -(complexNum& uno, complexNum& dos) {
-	double realSum = uno.getReal() - dos.getReal();
-	double imagSum = uno.getImaginary() - dos.getImaginary();
-	return complexNum(realSum, imagSum);
-}
-const complexNum operator /(complexNum& uno, complexNum& dos) {
-	double polarDiv = uno.getModulus() / dos.getModulus();
-	double phaseDiv = uno.getPhase() - dos.getPhase();
-
-	double realDiv = polarDiv * cos(phaseDiv);
-	double imagDiv = polarDiv * sin(phaseDiv);
-
-
-
-	return complexNum(realDiv, imagDiv);
-}
-const complexNum operator *(complexNum& uno, complexNum& dos) {
-	double polarDiv = uno.getModulus() * dos.getModulus();
-	double phaseDiv = uno.getPhase() + dos.getPhase();
-
-	double realDiv = polarDiv * cos(phaseDiv);
-	double imagDiv = polarDiv * sin(phaseDiv);
-
-
-
-	return complexNum(realDiv, imagDiv);
-}
-
-
-
-void printComplexNum(complexNum& printee) {
-	std::cout << "-------------";
-	std::cout << "\nReal: " << printee.getReal()
-		<< "\nImaginary: " << printee.getImaginary()
-		<< "\nModulus: " << printee.getModulus()
-		<< "\nPhase: " << printee.getPhase() << std::endl;
-	std::cout << "-------------";
-	std::cout << std::endl;
-}
+#include "complexNum.h"
 
 int main()
 {
@@ -78,6 +14,7 @@ int main()
 
 	c5 = c1 / c2;
 	c6 = c1 * c2;
+
 	printComplexNum(c3);
 	printComplexNum(c4);
 	printComplexNum(c5);
---
+++ P3/complexNum.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <iostream>
+class complexNum {
+public:
+	complexNum() : real(0), imaginary(0) {}
+	complexNum(double realIn, double imaginaryIn) : real(realIn), imaginary(imaginaryIn) {}
+
+	double getReal() {
+		return real;
+	}
+	double getImaginary() {
+		return imaginary;
+	}
+	double getModulus() {
+		return sqrt(pow(imaginary, 2) + pow(real, 2));
+	}
+	double getPhase() {
+		return atan2(imaginary, real);
+	}
+
+private:
+	double real = 0, imaginary = 0, absoluteVal = 0, congugate = 0;
+};
+const complexNum operator +(complexNum& uno, complexNum& dos) {
+	double realSum = uno.getReal() + dos.getReal();
+	double imagSum = uno.getImaginary() + dos.getImaginary();
+	return complexNum(realSum, imagSum);
+}
+const complexNum operator -(complexNum& uno, complexNum& dos) {
+	double realSum = uno.getReal() - dos.getReal();
+	double imagSum = uno.getImaginary() - dos.getImaginary();
+	return complexNum(realSum, imagSum);
+}
+const complexNum operator /(complexNum& uno, complexNum& dos) {
+	if (dos.getModulus() == 0) {
+		std::cerr << "Divide By zero is not mathematical nigga";
+		return complexNum();
+	}
+	double polarDiv = uno.getModulus() / dos.getModulus();
+	double phaseDiv = uno.getPhase() - dos.getPhase();
+
+	double realDiv = polarDiv * cos(phaseDiv);
+	double imagDiv = polarDiv * sin(phaseDiv);
+
+
+
+	return complexNum(realDiv, imagDiv);
+}
+const complexNum operator *(complexNum& uno, complexNum& dos) {
+	double polarDiv = uno.getModulus() * dos.getModulus();
+	double phaseDiv = uno.getPhase() + dos.getPhase();
+
+	double realDiv = polarDiv * cos(phaseDiv);
+	double imagDiv = polarDiv * sin(phaseDiv);
+
+
+
+	return complexNum(realDiv, imagDiv);
+}
+void printComplexNum(complexNum& printee) {
+	std::cout << "-------------";
+	std::cout << "\nReal: " << printee.getReal()
+		<< "\nImaginary: " << printee.getImaginary()
+		<< "\nModulus: " << printee.getModulus()
+		<< "\nPhase: " << printee.getPhase() << std::endl;
+	std::cout << "-------------";
+	std::cout << std::endl;
+}
